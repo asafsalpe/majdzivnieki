802876f99ab4012746bc76e1278cbe98e53fa414
asafsalpe <asafs.alpe@gmail.com>
2025-03-01 22:25:20 +0200

    asasasas

--- edieni.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".product").forEach(product => {
-      const bgImage = product.getAttribute("data-bg");
-      if (bgImage) {
-          product.style.backgroundImage = `url('${bgImage}')`;
-      }
-  });
-});
